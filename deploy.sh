bf5fd861d77c85483d96e6ec51b2aab66e8a0c09
shubham <devshubh2204@gmail.com>
2025-04-20 02:23:22 +0000

    update the code

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "🟡 Running Terraform..."
-cd terraform
+cd terraform-project
 terraform init
 terraform apply -auto-approve
 
 echo "🟢 Getting EC2 IP from Terraform output..."
-EC2_IP=$(terraform output -raw ec2_public_ip)
+EC2_IP=$(terraform output -raw public_ip)
 
 echo "🔵 Creating Ansible inventory.ini..."
 cat <<EOF > ../ansible/inventory.ini
 [jenkins]
-$EC2_IP ansible_user=ubuntu ansible_ssh_private_key_file=../terraform/Dev-hack
+$EC2_IP ansible_user=ubuntu ansible_ssh_private_key_file=../terraform-project/Dev-hack
 EOF
 
 echo "🟣 inventory.ini created with IP: $EC2_IP"
